34faea968ad8752052ce0afb1fd5c6c1596f8e39
Fabian Frangella <fabianfrangella@gmail.com>
2022-01-10 22:42:42 -0300

    fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const SQRepository = require('../src/repository/SQRepository')
-const { Criteria, CriteriaBuilder } = require('../src/query/Criteria')
+const SQRepository = require('./src/repository/SQRepository')
+const { Criteria, CriteriaBuilder } = require('./src/query/Criteria')
 
 module.exports = {
     SQRepository,
